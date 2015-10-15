eabb4e04e39a496d7d3729d9c6cc851296e8b65e
Yokoe <yokoe.hnut@gmail.com>
2015-10-15 16:25:48 +0900

    finish Chapter12Memory1 : Basic of Management Memory

--- Chapter12Memory1/Chapter12Memory1/main.m
+++ Chapter12Memory1/Chapter12Memory1/main.m
@@ -8,6 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+@interface ArrangedString : NSObject
+@end
+
+@implementation ArrangedString
+- (void)dealloc
+{
+    [super dealloc] ;
+    NSLog(@"参照カウンタ値は 0 になり、メモリから解放されました") ;
+}
+@end
+
 int main(int argc, const char * argv[])
 {
     @autoreleasepool
@@ -22,6 +33,29 @@ int main(int argc, const char * argv[])
          *** また、その機能があるので、以前よりはメモリ管理について開発者が神経質にならず済む、通常は ON にしよう ***/
 
         NSLog(@"string の参照カウンタ値は %ld \n" , [string retainCount]) ;
+        NSLog(@"1 : %@\n", string) ;
+        [string retain] ;
+        NSLog(@"string の参照カウンタ値は %ld \n" , [string retainCount]) ;
+        NSLog(@"2 : %@\n", string) ;
+        [string release] ;
+        NSLog(@"string の参照カウンタ値は %ld \n" , [string retainCount]) ;
+        NSLog(@"3 : %@\n", string) ;
+        [string release] ;
+        
+        // ここで参照カウンタ値は 0 になり、インスタンス string はメモリから解放され、値を取り出せなくなる
+        // （以下 2行のようなコードは、危険なので書かないべきである）
+        // NSLog(@"string の参照カウンタ値は %ld \n" , [string retainCount]) ;
+        // NSLog(@"4 : %@\n", string) ;
+        
+        
+        // dealloc をオーバーライドしてみた
+        ArrangedString *arrangedString = [[ArrangedString alloc] init] ;
+        NSLog(@"string の参照カウンタ値は %ld \n" , [arrangedString retainCount]) ;
+        
+        // 型名を取得できる関数。便利。
+        NSLog(@"%@ \n", NSStringFromClass([arrangedString class]) ) ;
+        [arrangedString release] ;
+        
     }
     return 0 ;
 }
